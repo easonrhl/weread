834c9b082e64915b669134a3eeaaeda6441db85c
eason <1262176119@qq.com>
2021-02-17 20:51:17 +0800

    修改baseURL

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'http://127.0.0.1:3000/api',
-  baseURL: 'http://192.168.43.218:3000/api',
+  baseURL: 'http://127.0.0.1:3000/api',
   timeout: 5000
 })
 
